4b6f7e2857620a94d16a30ba4a1404992653012b
DESKTOP-LRU7KSA\Krzysiek <k.poros@gmail.com>
2022-05-08 21:06:47 +0200

    We are rotating towards the pointer

--- Assets/_FatCat/Scripts/Cat Movement/CatMovement.cs
+++ Assets/_FatCat/Scripts/Cat Movement/CatMovement.cs
@@ -6,9 +6,15 @@ using UnityEngine;
 public class CatMovement : MonoBehaviour
 {
     [SerializeField] float _movementSpeed = 1.0f;
-    [SerializeField] float _rotationSpeed = 1.0f;    
+    [SerializeField] float _rotationSpeed = 1.0f;
 
+    Transform _myTransform = null;
     IInputReader _inputReader = null;
+
+    private void Awake()
+    {
+        _myTransform = this.transform;
+    }
 
     private void Start()
     {
@@ -18,14 +24,24 @@ public class CatMovement : MonoBehaviour
     private void FixedUpdate()
     {
         // check if mouse is properly placed, allow moving there
+        // check if can move, collision or something?
 
-        var pos = _inputReader.InputWorldPos;
-        var flatPos = new Vector3(pos.x, transform.position.y, pos.z);
-
-        // rotate a little towards the point
+        RotateToPointer();
 
         // add force in the forward direction
 
         // we need
+    }
+
+    private void RotateToPointer()
+    {
+        var pos = _inputReader.InputWorldPos;
+        var flatPos = new Vector3(pos.x, transform.position.y, pos.z);
+        var directionToTarget = flatPos - _myTransform.position;
+        var targetRotation = Quaternion.LookRotation(directionToTarget);
+
+        // we should freeze RB?
+        _myTransform.rotation = Quaternion.Slerp(_myTransform.rotation, targetRotation, _rotationSpeed);
+        // we can unfreeze the RB now
     }
 }
