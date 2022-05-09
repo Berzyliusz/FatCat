f020d5fa4c3c1404bc594da0fed877caba70ca83
DESKTOP-LRU7KSA\Krzysiek <k.poros@gmail.com>
2022-05-09 18:46:06 +0200

    Planning mouse stuff

---
+++ Assets/_FatCat/Scripts/Mouse Scripts/MouseSpawner.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseSpawner : MonoBehaviour
+{
+    // Spawn mice over time from some predesignated areas
+}
